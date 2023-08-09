e4837a5c60b1314e6ddb09550005847617c87983
Gaetano Vallarelli <biroplane@users.noreply.github.com>
2023-08-09 11:02:46 +0200

    Change date

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="my-6">
     <div
-      class="w-full h-full flex flex-col items-center justify-center container px-8"
+      class="container flex flex-col items-center justify-center w-full h-full px-8"
     >
       <img src="/img/logo.png" class="w-64" />
-      <h1 class="font-bold text-red-700 text-5xl mt-12">BIRRAMILANDIA</h1>
-      <h2 class="font-bold text-red-700 text-3xl">Settembre 2023</h2>
+      <h1 class="mt-12 text-5xl font-bold text-red-700">BIRRAMILANDIA</h1>
+      <h2 class="text-3xl font-bold text-red-700">Ottobre 2023</h2>
     </div>
     <!-- <ContactForm /> -->
   </div>
